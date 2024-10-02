7cd003e437ef695693b61dc2fab15acb5abcff82
KidHarlesh <123456789+odin@users.noreply.github.com>
2024-10-02 12:31:38 -0700

    new update

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-var image = document.getElementById("imageone");
-function changeimage() {
-  image.src = "image/approved_2951459.png";
+const checkbox = document.getElementById("checkboxs");
+const image = document.getElementById("imageone");
+function toogleImage() {
+  if (checkbox.checked) {
+    checkbox.style.display = "none";
+    image.style.display = "block";
+  } else {
+    checkbox.style.display = "block";
+    image.style.display = "none";
+    checkbox.checked = true;
+  }
 }
+// checkbox.addEventListener("click", toogleImage);
+// image.addEventListener("click", toogleImage);
+// } else if(image=""=="image/approved_2951459.png")
+//   checkbox.style.display = "block";
+//   image.style.display = "none";
+
+// function
+//  if(image==="image/approved_2951459.png"){
+//   checkbox.style.display="block"
+//   image.style.display=
+//  }
